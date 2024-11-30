3f9c841c2acea24562c33a9ad4384f72b3306087
nathan <nchaddy50@protonmail.com>
2024-11-30 14:09:59 -0600

    Show album artwork in track browser

--- src/components/MainPane/TrackBrowser.tsx
+++ src/components/MainPane/TrackBrowser.tsx
@@ -6,20 +6,35 @@ interface TrackBrowserProps {
 }
 
 function TrackBrowser(props: TrackBrowserProps) {
-const { album } = props;
-const [tracks, setTracks] = useState([""]);
+    const { album } = props;
+    const [tracks, setTracks] = useState([""]);
+    const [artworkSource, setArtworkSource] = useState("");
 
-useEffect(() => {
-async function getTracksForAlbum(album: string): Promise<void> {
-    const tracks: string[] = await invoke("get_tracks_for_album", { album });
-    setTracks(tracks);
-}
-
-getTracksForAlbum(album);
-}, [album]);
+    useEffect(() => {
+        async function getTracksForAlbum(album: string): Promise<void> {
+            const tracks: string[] = await invoke("get_tracks_for_album", {
+                album,
+            });
+            setTracks(tracks);
+        }
+        async function getAlbumArtwork(album: string): Promise<void> {
+            const artworkSource: string = await invoke(
+                "get_artwork_for_album",
+                {
+                    album,
+                }
+            );
+            setArtworkSource(artworkSource);
+        }
+        getTracksForAlbum(album);
+        getAlbumArtwork(album);
+    }, [album]);
 
     return (
         <div>
+            <div>
+                <img src={artworkSource} height="100px" width="100px" />
+            </div>
             {tracks.map((track, index) => {
                 return <p>{track}</p>;
             })}
