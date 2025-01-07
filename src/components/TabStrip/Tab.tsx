f6d16f6384b62c5cdd2845c5603a8a2ae6ad889e
nathan <nchaddy50@protonmail.com>
2025-01-06 21:40:19 -0600

    Make cursor pointer where appropriate

--- src/components/TabStrip/Tab.tsx
+++ src/components/TabStrip/Tab.tsx
@@ -10,15 +10,7 @@ function Tab(props: TabProps) {
     const { title, isSelected, selectTab } = props;
 
     return (
-        <div
-            style={{
-                paddingLeft: "10px",
-                paddingRight: "10px",
-                borderLeft: "1px solid black",
-                borderRight: "1px solid black",
-            }}
-            onClick={selectTab}
-        >
+        <div className="tab" onClick={selectTab}>
             <p className={isSelected ? "selectedTab" : "unselectedTab"}>
                 {title}
             </p>
